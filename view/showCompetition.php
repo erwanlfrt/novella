e3a22033ba6cad1abe25e99e07815414b60467e8
Erwan Le Forestier <erwan.leforestier@orange.com>
2021-06-23 12:34:21 +0200

    travail incroyable de Erwan

--- view/showCompetition.php
+++ view/showCompetition.php
@@ -6,13 +6,33 @@ if (!$_SESSION) {
 
 use \model\tables\Competition;
 use \model\tables\RequiredWord;
+use \model\tables\Jury;
+use \model\tables\Prejury;
 
 $dataAccess = new Competition;
 $requiredWordAccess = new RequiredWord;
+$juryAccess = new Jury;
+$prejuryAccess = new Prejury;
 
 if (isset($_GET['id'])) {
   $competition = $dataAccess->getCompetition($_GET['id']);
   $requiredWords = $requiredWordAccess->getRequiredWords($_GET['id']);
+  $isJury = mysqli_fetch_array($juryAccess->getCompetition($_GET['id'], $_SESSION['email']))["points"] != null;
+  $isPrejury = mysqli_fetch_array($prejuryAccess->getCompetition($_GET['id'], $_SESSION['email']))["points"] != null;
+
+  $origin = new DateTime("now");
+  $target = new DateTime($competition['deadline']);
+  $interval = $origin->diff($target);
+  $isCompetitionOver = $interval->invert > 0;
+  if($isCompetitionOver) {
+    $remainingTime = $interval->format('(terminé depuis %a jours)');
+  }
+  else {
+    $remainingTime = $interval->format('(%a jours restants)');
+  }
+  
+  $properDate = $target->format("d / m / Y");
+
 }
 ?>
 <!DOCTYPE html>
@@ -34,16 +54,29 @@ if (isset($_GET['id'])) {
       <h2 class="infos__title">Incipit :</h2>
       <p class="infos__contenu"><?php echo $competition['incipit'] ?></p>
       <h2 class="infos__title">Deadline :</h2>
-      <p class="infos__contenu"><?php echo $competition['deadline'] ?></p>
+      <p class="infos__contenu"><?php echo $properDate." ".$remainingTime ?></p>
       <h2 class="infos__title">Mots requis :</h2>
       <p class="infos__contenu" id="motsRequis"><?php
-                                                while ($data = mysqli_fetch_array($requiredWords)) {
-                                                  echo $data[0] ?>, <?php
-                                                } ?></p>
+          $count = 0;
+          while ($data = mysqli_fetch_array($requiredWords)) {
+            $count++;
+            echo $data[0] ?>, <?php
+          }
+          ?></p>
+          <?= $count == 0 ? "aucun mot requis" : "" ?>
     </div>
 
     <form class="form" action="?action=participate&id=<?php echo $competition['id'] ?>" method="POST">
-      <input class="form__login__submit" type="submit" value="Participer" />
+      <p style="color: red">
+      <?php 
+        if($isJury || $isPrejury) {
+          echo "En tant que membre du ".($isPrejury? "pre" : "")."jury vous n'êtes pas autorisé à participer à ce concours<br>";
+        }
+        if($isCompetitionOver) {
+          echo "concours terminé, vous ne pouvez plus y participer";
+        }
+      ?> </p>
+      <input class="form__login__submit" type="submit" value="Participer" <?= ($isPrejury || $isJury || $isCompetitionOver) ? "disabled" : ""?> />
     </form>
   </main>
 </body>
